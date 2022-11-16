4eca3bd81a113e7c9cde00e19feba178b1b08c98
RiivoI <113367497+RiivoI@users.noreply.github.com>
2022-11-16 15:09:57 +0200

    matkaklubi

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Navigatsioon from "../components/Navigatsioon";
+import contactUs from "./contactUs";
+
 
 const Pais = () => {
   return (
@@ -21,8 +23,54 @@ const Pais = () => {
 
 const Teine = () => {
   return (
-    <div>
-      TEINE
+    <div className={styles.teine}>
+    <section className={styles.ylemine}>
+     <h3 className={styles.hall}>Choose</h3>
+     <h3>These Best Treks For Spring</h3>
+    </section>
+    <div className={styles.alumine}>
+        <div className={styles.vasak}>
+          <section>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Quis ipsum su spendisse ultrices gravida. Risus commodo viverra maec enas
+                accumsan lacus vel facilisis.
+         </section>
+          <div className={styles.nooled}>
+            <span className={styles.vasak}>←</span>
+            <span clasName={styles.lehekylgVasak}>01</span>
+            <span className={styles.kaldkriips}>/</span>
+            <span className={styles.lehekylgParem}>06</span>
+            <span clasName={styles.parem}>→</span>
+         </div>
+        </div>
+        <div className={styles.parem}>
+         <div className={`${styles.pilt} ${styles.pilt1}`}></div>
+         <div className={`${styles.pilt} ${styles.pilt2}`}></div>
+         <div className={`${styles.pilt} ${styles.pilt3}`}></div>
+        </div>
+    </div>
+</div>
+  )
+}
+
+const Jalus = () => {
+  return (
+    <div className={styles.jalus}>
+      <Navigatsioon />
+      <div className={styles.alumineRida}>
+        <div className={styles.alumine}>
+                <div className={styles.tekst}>
+                <h5>CALL NOW</h5>
+                <br></br>
+                <h6>1800 888 555</h6>
+        </div>
+        <div className={styles.keskmine}>
+                <section>
+                    © Company Name 2020. All rights reserved.
+                </section>
+        </div>
+      </div>.
+      </div>
     </div>
   )
 }
@@ -32,10 +80,11 @@ const Home = () => {
     <div>
       <Head>
         <title>Matkaklubi</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/Logo.ico" />
       </Head>
       <Pais />
       <Teine />
+      <Jalus />
     </div>
   );
 };
